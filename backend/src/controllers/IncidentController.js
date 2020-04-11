622568ba25804be90bf7ae472ef45898d46b927e
Enya <enyalgs@yahoo.com>
2020-04-11 00:56:59 -0300

    Frontend - Web com react

--- backend/src/controllers/IncidentController.js
+++ backend/src/controllers/IncidentController.js
@@ -31,7 +31,7 @@ module.exports = {
         const { title, description, value } = request.body;
         const ong_id = request.headers.authorization;
 
-        const [id] = await connection('incidents').insert({
+        const [ id ] = await connection('incidents').insert({
             title,
             description,
             value,
